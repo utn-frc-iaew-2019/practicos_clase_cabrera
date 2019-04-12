a76fad26df511a15c08d603646350c8a91cfb993
gaston67051 <gasty065@gmail.com>
2019-04-12 17:53:33 -0300

    proyecto

---
+++ work-desde-cero/Practica/src/main/java/Cabrera/Practica/App.java
@@ -0,0 +1,13 @@
+package Cabrera.Practica;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
